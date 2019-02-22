31550f574e6f20d5bd3d00f754c303ca13330c53
Alexandros Kordatzakis <alexkordatzakis2@outlook.com.gr>
2019-02-22 17:40:33 +0200

    Updated a lot of features.

--- js/main.js
+++ js/main.js
@@ -35,6 +35,19 @@ $(document).ready(function() {
 			})
 		}
 	});
+    
+    window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+  top: 100, // could be negative value
+  left: 0, 
+  behavior: 'smooth' 
+});
 
 
 	// Price Table
